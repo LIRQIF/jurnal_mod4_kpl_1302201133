5f3c47b63a8ea47b7e5f7c448e75865bb67468c8
fiqril <mfikrilh@gmail.com>
2022-03-19 16:25:23 -0700

    coba lagi

--- modul4_1302201133/Program.cs
+++ modul4_1302201133/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var simpleDataBase = new SimpleDataBase<int>();
+            simpleDataBase.AddNewData(12);
+            simpleDataBase.AddNewData(34);
+            simpleDataBase.AddNewData(56);
+            simpleDataBase.PrintAllData();
+        }
+    }
+}
---
+++ modul4_1302201133/SimpleDataBase.cs
@@ -0,0 +1,30 @@
+﻿public class SimpleDataBase
+<T>
+{
+    List<T> storedData;
+    List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        int count = 0;
+        storedData.ForEach(delegate (T data)
+        {
+            Console.WriteLine($"Data {count + 1} berisi: {data}, yang disimpan pada waktu UTC: {inputDates[count]}");
+            count++;
+        });
+    }
+
+
+}
